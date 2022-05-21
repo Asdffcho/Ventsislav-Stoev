2113d15bae26d344db19cd3909f0ed42f35c5910
Asdffcho <ventsistoev7@gmail.com>
2022-05-21 17:41:20 +0300

    test

---
+++ Names.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Names {
+
+
+    public static class WholeName {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter your fist name: ");
+            String firstName = scanner.nextLine();
+
+            //       scanner = new Scanner(System.in);
+            System.out.println("Enter your middle name: ");
+            String middleName = scanner.nextLine();
+
+//        scanner = new Scanner(System.in);
+            System.out.println("Enter your last name: ");
+            String lastName = scanner.nextLine();
+
+            System.out.println("My full name is: "+ firstName + " " + middleName + " " + lastName);
+
+        }
+    }
+}
