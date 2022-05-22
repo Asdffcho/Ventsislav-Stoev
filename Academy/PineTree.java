19a5267512965fbcf17b2e0f5a31d1cfc31ada46
Asdffcho <ventsistoev7@gmail.com>
2022-05-22 15:53:38 +0300

    Manteromate

---
+++ Academy/PineTree.java
@@ -0,0 +1,16 @@
+public class PineTree {
+    public static void main(String[] args) {
+
+
+        String String1 = "*";
+        System.out.println(" " + " " + String1);
+
+
+        String String2 = ("* *");
+        System.out.println(" " + String2);
+
+
+        String String3 = ("* * *");
+        System.out.println(String3);
+    }
+}
